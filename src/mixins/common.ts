e04909bae54d1bee609c37778c415c44268a0ae5
Viral Patel <viralpatel508@gmail.com>
2022-04-09 13:23:10 +0530

    refactor: removed css variables

---
+++ src/mixins/common.ts
@@ -0,0 +1,27 @@
+const CommonMixin = {
+    methods: {
+        todayDate() {
+            const months = [
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
+            ];
+            const date = new Date();
+            const month = months[date.getMonth()];
+            const day = date.getDate();
+            const year = date.getFullYear();
+            return month + " " + day + ", " + year;
+        },
+    }
+}
+
+export default CommonMixin;
--- src/views/Task.vue
+++ src/views/Task.vue
@@ -2,6 +2,25 @@
   <ion-page>
     <ion-content :fullscreen="true">
       <topbar :toolbar-title="toolbarTitle" />
+      <div
+        class="
+          c-topbar-bottom
+          d-flex
+          align-items-center
+          justify-content-between
+        "
+      >
+        <div class="c-topbar-bottom__left">
+          <div class="c-topbar-bottom__date">{{ todayDate() }}</div>
+          <strong>Today</strong>
+        </div>
+        <div class="topbar-bottom__right">
+          <ion-button mode="md" href="/tabs/task/add" color="secondary">
+            <ion-icon slot="start" :icon="addOutline" />
+            <span>Add Task</span>
+          </ion-button>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -12,12 +31,16 @@ import { IonPage, IonContent } from "@ionic/vue";
 
 import { Constants } from "@/constants/index";
 import Topbar from "@/components/topbar/Topbar.vue";
+import { addOutline } from "ionicons/icons";
+import CommonMixin from "@/mixins/common";
 export default defineComponent({
   name: Constants.NAME.TASK_TAB,
   components: { IonContent, IonPage, Topbar },
+  mixins: [CommonMixin],
   setup() {
     return {
       toolbarTitle: "Task",
+      addOutline,
     };
   },
 });
